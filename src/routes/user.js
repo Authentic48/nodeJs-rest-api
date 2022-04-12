ef402614dc59a95c4d2bdf8c6593c10b6260273e
Authentic48 <iboufall48@gmail.com>
2022-04-12 10:57:37 +0700

    Signin reoute refactored
    The signin process was redesigned. Email and password validation was added, user  existance  check with a given email and password, if password is invalid we throw an error otherwise we persue the signin by creating a jwt token.  and send it back along with user.id and email.

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-const { body } = require('express-validator');
-const checkAuth = require("../middleware/check-auth");
-const errorHandler = require('../middleware/error-middleware');
-const validationResult = require('express-validator');
-
+const { body } = require("express-validator");
+const validationResult = require("express-validator");
 
 const router = express.Router();
 
-// signup 
+// signup
 router.post(
-    '/signup',
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage("Password must be between 4 and 20 characters"),
-  async  (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions  
+  "/signup",
+  body("email").isEmail().withMessage("Email must be valid"),
+  body("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Password must be between 4 and 20 characters"),
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -32,99 +28,72 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new Error('User already exist');
+      throw new Error("User already exist");
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-        email,
-        password: hashedPassword
-    })
+      email,
+      password: hashedPassword,
+    });
 
     const userJwt = jwt.sign(
-        {
-          id: user.id,
-          email: user.email,
-        },
-        JWT_SECRET_KEY
-      );
+      {
+        id: user.id,
+        email: user.email,
+      },
+      JWT_SECRET_KEY
+    );
 
     res.status(201).json({
-            id: user._id,
-            email: user.email,
-            token: userJwt
-    })
-
+      id: user._id,
+      email: user.email,
+      token: userJwt,
+    });
   }
 );
 
+// Login
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("Email must be valid"),
+  body("password").trim().notEmpty().withMessage("Password must be supplied"),
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    const { email, password } = req.body;
 
+    const existingUser = await User.findOne({ email });
 
-//Login
-// router.post('/login', (req, res) => {
+    if (!existingUser) {
+      throw new Error("Invalid credentials");
+    }
 
-//     User.find({ email: req.body.email })
-//         .exec()
-//         .then(user => {
-//             if (user.length < 1) {
-//                 return res.status(404).json({
-//                     message: 'Auth Failed'
-//                 });
-//             }
+    const validPassword = await bcrypt.compare(password, user.password);
 
-//             bcrypt.compare(req.body.password, user[0].password, (err, resPassCompare) => {
-//                 if (!resPassCompare || err) {
-//                     return res.status(401).json({
-//                         message: 'Auth Failed'
-//                     });
-//                 }
+    if (!validPassword) {
+      throw new Error("Invalid credentials");
+    }
 
-//                 if (resPassCompare) {
-//                     const token = jwt.sign({
-//                         email: user[0].email,
-//                         userId: user[0]._id
-//                     }, JWT_KEY, {
-//                         expiresIn: "1h"
-//                     });
-//                     return res.status(200).json({
-//                         message: 'Auth Succefull',
-//                         token
-//                     });
-//                 }
-//             });
-//         })
-//         .catch(err => {
-//             res.status(500).json({
-//                 error: err
-//             })
-//         })
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_SECRET_KEY
+    );
 
-
-
-// });
-
-
-
-// router.delete('/:userId', checkAuth, (req, res) => {
-//     const userId = mongoose.Types.ObjectId(req.params.userId)
-
-//     User.deleteOne({ "_id": userId })
-//         .exec()
-//         .then(deletedUserStatus => {
-//             res.status(200).json({
-//                 msg: 'User Deleted Succesfully...',
-//                 deletedUserStatus
-//             })
-//         })
-//         .catch(err => {
-//             res.status(500).json({
-//                 error: err
-//             });
-//         });
-// })
-
-
+    res.status(200).json({
+      id: existingUser._id,
+      email: existingUser.email,
+      token: userJwt,
+    });
+  }
+);
 
 module.exports = router;
