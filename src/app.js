9992eac6e607bb531179bd68c31f6cc9f4b088de
Authentic48 <iboufall48@gmail.com>
2022-04-12 11:08:44 +0700

    App.js refactored again
    I cleaned up the app.js file again by removing unnecessary imported function. I have also created some error handler middleware notFound for undefined routes and errorHandler to catch any error message and send it back as a response

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const productRoutes = require('./routes/products')
 const orderRoutes = require('./routes/orders')
 const userRoutes = require('./routes/user')
 const colors = require('colors')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const { errorHandler, notFound } = require('./middleware/error-middleware')
 
 const app = express();
 
@@ -22,37 +22,14 @@ app.use(express.urlencoded({ extended: true }));
 // logging middleware
 app.use(morgan('dev'))
 
-//Add heders
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header', '*');
-    if (req.method === 'OPTIONS') {
-        req.header('Access-Control-Allow-Methods', 'POST,PUT,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-})
-
-
 // Routes
 // app.use('/products', productRoutes);
 // app.use('/orders', orderRoutes);
 app.use('/user', userRoutes);
 
-//Fallback routes
-
-app.use((req, res, next) => {
-    const error = new Error(`Not found your route :${req.hostname}, what you are looking for...`);
-    error.status = 404;
-    next(error);
-});
-
-// Trigger changes
-app.use((error, req, res, next) => {
-    res.status(error.status || 500).json({
-        msg: error.message
-    });
-});
+// Error Middlewares
+app.use(errorHandler)
+app.use(notFound)
 
 
 const PORT = process.env.PORT || 5000 
